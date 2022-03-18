ccaa2499e2797161f443f9302cffcd49ed930a2f
Stan Sebastian <stanadrian717@gmail.com>
2022-03-18 20:07:40 +0100

    Progress of 18/03/22

--- src/controller/DbManager.java
+++ src/controller/DbManager.java
@@ -4,10 +4,9 @@ package controller;
 import model.db.DbContext;
 import model.exception.NotImplementedException;
 
-   /*
-    *Controlador para linkear la BD
-    */
-
+/**
+ * Controlador para linkear la BD
+ */
 public class DbManager {
     public void connect(DbContext dbContext) throws NotImplementedException {
         throw new NotImplementedException();
--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -9,16 +9,9 @@ import java.awt.*;
 public class MainMenu extends JFrame implements IMenu {
     private JPanel panelMain;
     private JButton button1;
-    private JButton button2;
-    private JButton button3;
 
     public MainMenu() {
         super("Pandemic - V1rulent");
-        initialize();
-    }
-
-    public MainMenu(String title) {
-        super(title);
         initialize();
     }
 
