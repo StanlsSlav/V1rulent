2f24f0c7abb8592afaf1e5729c5ac866a826774d
Stan Sebastian <stanadrian717@gmail.com>
2022-03-23 23:14:07 +0100

    Add buttons layout

--- src/model/base/Button.java
+++ src/model/base/Button.java
@@ -3,6 +3,9 @@ package model.base;
 
 import javax.swing.JButton;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import static java.awt.Font.PLAIN;
 
@@ -11,7 +14,23 @@ public class Button extends JButton {
         initialize();
     }
 
+    public Button(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
     private void initialize() {
         setFont(new Font("Roboto Light", PLAIN, 36));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
     }
 }
