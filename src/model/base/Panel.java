d22c1834e9c342df1b22e3fe267b273de03f1b96
Stan Sebastian <stanadrian717@gmail.com>
2022-03-23 20:11:35 +0100

    Progress of 23/03/22

--- src/model/base/Panel.java
+++ src/model/base/Panel.java
@@ -5,16 +5,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panel extends JPanel {
-    private Image backgroundImage;
+    public Panel() {
+    }
 
     public Panel(Image backgroundImage) {
         this.backgroundImage = backgroundImage;
     }
 
+    private Image backgroundImage;
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        g.drawImage(backgroundImage, 0, 0, null);
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
     }
 }
