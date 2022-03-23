029f973e0924593112e0f560e4967ef9fd516161
Stan Sebastian <stanadrian717@gmail.com>
2022-03-23 20:11:35 +0100

    Progress of 23/03/22

--- src/model/base/Button.java
+++ src/model/base/Button.java
@@ -2,12 +2,16 @@ package model.base;
 
 
 import javax.swing.JButton;
+import java.awt.Font;
+
+import static java.awt.Font.PLAIN;
 
 public class Button extends JButton {
     public Button() {
+        initialize();
     }
 
-    public Button(String content) {
-        setText(content);
+    private void initialize() {
+        setFont(new Font("Roboto Light", PLAIN, 36));
     }
 }
--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -1,13 +1,25 @@
 package view;
 
 
+import model.base.Button;
+import model.base.Panel;
 import model.interfaces.IMenu;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class MainMenu extends JFrame implements IMenu {
-    private JPanel panelMain;
+    private JLabel menuTitle;
+
+    private JPanel mainPanel;
+    private JPanel buttonsPanel;
+
+    private JButton playButton;
+    private JButton rulesButton;
+    private JButton settingsButton;
+    private JButton creditsButton;
+    private JButton exitButton;
 
     public MainMenu() {
         super("Pandemic - V1rulent");
@@ -17,10 +29,20 @@ public class MainMenu extends JFrame implements IMenu {
     @Override
     public void initialize() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new Dimension(800, 600));
+        setMinimumSize(new Dimension(960, 540));
 
-        add(panelMain);
+        add(mainPanel);
         pack();
         setVisible(true);
+    }
+
+    private void createUIComponents() {
+        mainPanel = new Panel(Toolkit.getDefaultToolkit().createImage("./src/assets/MainMenuBg.png"));
+
+        playButton = new Button();
+        rulesButton = new Button();
+        settingsButton = new Button();
+        creditsButton = new Button();
+        exitButton = new Button();
     }
 }
