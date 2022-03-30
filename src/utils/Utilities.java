0b0d8a290aa14b76143fd40379089d780275a5c9
Stan Sebastian <stanadrian717@gmail.com>
2022-03-30 20:11:57 +0200

    Progress of 30/03/22

--- src/utils/Utilities.java
+++ src/utils/Utilities.java
@@ -6,14 +6,13 @@ import java.awt.CardLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-
 public class Utilities {
     public static MouseAdapter exitOnClick = new MouseAdapter() {
         @Override
         public void mousePressed(MouseEvent e) {
             super.mousePressed(e);
 
-            if (e.getButton() == MouseEvent.BUTTON1) {
+            if (isLeftButtonPressed(e)) {
                 System.exit(0);
             }
         }
@@ -22,5 +21,9 @@ public class Utilities {
     public static void switchToCard(JPanel rootPanel, String cardName) {
         CardLayout cardLayout = (CardLayout) rootPanel.getLayout();
         cardLayout.show(rootPanel, cardName);
+    }
+
+    public static boolean isLeftButtonPressed(MouseEvent event) {
+        return event.getButton() == MouseEvent.BUTTON1;
     }
 }
