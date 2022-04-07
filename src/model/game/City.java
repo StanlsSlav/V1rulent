32a1088b19688fa8977b698f7fe977c7a743a1ae
Stan Sebastian <stanadrian717@gmail.com>
2022-04-07 15:52:03 +0200

    Add color to City.java

--- src/model/game/City.java
+++ src/model/game/City.java
@@ -6,13 +6,22 @@ import model.Position;
 import java.util.ArrayList;
 import java.util.Dictionary;
 
+
 /**
  * Una ciudad de la mapa
  */
 public class City {
     public String name;
+    public Color color;
     public Position position;
     public ArrayList<City> connectedCities;
     public Dictionary<Illness, Integer> presentIllnesses;
     public int totalPopulation;
+
+    public enum Color {
+        Blue,
+        Red,
+        Green,
+        Yellow
+    }
 }
