22411de21144325c63d3053f87f2f4e7def8d3f2
Sebastian Stan <stanadrian717@gmail.com>
2022-04-21 21:16:56 +0200

    Progress of 21/04/22

--- src/model/game/City.java
+++ src/model/game/City.java
@@ -6,21 +6,67 @@ import model.Location;
 import java.util.ArrayList;
 import java.util.Dictionary;
 
-
 /**
  * Una ciudad de la mapa
  */
 public class City {
-    public String name;
-    public Color color;
-    public Location location;
-    public ArrayList<City> connectedCities;
-    public Dictionary<Illness, Integer> presentIllnesses;
-
     public enum Color {
         Blue,
         Red,
         Green,
         Yellow
+    }
+
+    public City() {
+    }
+
+    public City(Location location) {
+        setLocation(location);
+    }
+
+    private String name;
+    private Color color;
+    private Location location;
+    private ArrayList<City> connectedCities;
+    private Dictionary<Illness, Integer> presentIllnesses;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public ArrayList<City> getConnectedCities() {
+        return connectedCities;
+    }
+
+    public void setConnectedCities(ArrayList<City> connectedCities) {
+        this.connectedCities = connectedCities;
+    }
+
+    public Dictionary<Illness, Integer> getPresentIllnesses() {
+        return presentIllnesses;
+    }
+
+    public void setPresentIllnesses(Dictionary<Illness, Integer> presentIllnesses) {
+        this.presentIllnesses = presentIllnesses;
     }
 }
--- src/utils/Utilities.java
+++ src/utils/Utilities.java
@@ -1,7 +1,10 @@
 package utils;
 
 
+import model.Location;
 import model.base.Panel;
+import model.game.City;
+import model.game.Map;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,9 +14,20 @@ import java.awt.Toolkit;
 import java.awt.Window;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Utilities {
+    public static boolean isLeftButtonPressed(MouseEvent event) {
+        return event.getButton() == MouseEvent.BUTTON1;
+    }
+
     public static MouseAdapter exitOnClick = new MouseAdapter() {
         @Override
         public void mousePressed(MouseEvent e) {
@@ -31,7 +45,7 @@ public class Utilities {
 
         // Centra la ventana del juego al centro del monitor
         Window.getWindows()[0].setLocation((int) x / 2 - mainFrame.getWidth() / 2,
-                                           (int) y / 2 - mainFrame.getHeight() / 2);
+              (int) y / 2 - mainFrame.getHeight() / 2);
     }
 
     public static void switchImage(JPanel panel, Image image) {
@@ -47,7 +61,68 @@ public class Utilities {
         cardLayout.show(rootPanel, cardName);
     }
 
-    public static boolean isLeftButtonPressed(MouseEvent event) {
-        return event.getButton() == MouseEvent.BUTTON1;
+    public static void loadCities() {
+        File citiesFile = new File("src/assets/cities.csv");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(citiesFile))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] dotCommaSplit = line.split(";");
+                String color = dotCommaSplit[1];
+                String[] location = dotCommaSplit[2].split(",");
+
+                City city = new City();
+
+                city.setName(dotCommaSplit[0]);
+                city.setLocation(new Location(Integer.parseInt(location[0]), Integer.parseInt(location[1])));
+                city.setColor(City.Color.valueOf(color));
+
+                Map.getInstance().cities.add(city);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (EOFException ignored) {
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        loadCityConnections();
+    }
+
+    private static void loadCityConnections() {
+        File citiesFile = new File("src/assets/cities.csv");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(citiesFile))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] dotCommaSplit = line.split(";");
+                String cityName = dotCommaSplit[0];
+                String[] connectedCitiesNames = dotCommaSplit[3].split(",");
+                ArrayList<City> connectedCities = new ArrayList<>();
+
+                City toModify = Map.getInstance()
+                      .cities.stream()
+                      .filter(x -> x.getName().equals(cityName))
+                      .findFirst()
+                      .orElseThrow(() -> new RuntimeException(String.format("'%s' was not found", cityName)));
+
+                for (String connectedCity : connectedCitiesNames) {
+                    connectedCities.add(Map.getInstance()
+                          .cities.stream()
+                          .filter(x -> x.getName().equals(connectedCity))
+                          .findFirst()
+                          .orElseThrow(() -> new RuntimeException(String.format("'%s' was not found", connectedCity))));
+                }
+
+                toModify.setConnectedCities(connectedCities);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (EOFException ignored) {
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
