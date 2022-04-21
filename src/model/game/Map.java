a57bfb94884d9b040fc7751333e804f0fba99a0a
Sebastian Stan <stanadrian717@gmail.com>
2022-04-21 20:31:26 +0200

    Add singleton for Map.java

--- src/model/game/Map.java
+++ src/model/game/Map.java
@@ -7,6 +7,16 @@ import java.util.ArrayList;
  * Representante del mapa del mundo (juego)
  */
 public class Map {
+    public static Map instance;
+
+    public static Map getInstance() {
+        if (null == instance) {
+            instance = new Map();
+        }
+
+        return instance;
+    }
+
     public ArrayList<City> cities;
 
     public static int citiesBetween(City startingPoint, City destination) throws Exception {
