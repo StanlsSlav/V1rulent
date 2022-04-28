8e53cef98738a0e6a2f62e6cc7e6bc7e9cccd2a8
Stan Sebastian <stanadrian717@gmail.com>
2022-04-28 13:36:17 +0200

    Add optional actions logging

--- src/model/Logger.java
+++ src/model/Logger.java
@@ -17,11 +17,19 @@ public class Logger {
 
     @SafeVarargs
     public final <T> void log(String message, T... parameters) {
+        log(true, message, parameters);
+    }
+
+    @SafeVarargs
+    public final <T> void log(boolean logActionsLeft, String message, T... parameters) {
         for (T parameter : parameters) {
             message = message.replaceFirst("%p", parameter.toString());
         }
 
-        MainMenu.getInstance().historialTxtArea.append("\n" + message + "\n");
-        MainMenu.getInstance().historialTxtArea.append(Player.getInstance().actions + " actions left\n");
+        MainMenu.getInstance().historialTxtArea.append(message + "\n");
+
+        if (logActionsLeft) {
+            MainMenu.getInstance().historialTxtArea.append(Player.getInstance().actions + " actions left\n");
+        }
     }
 }
