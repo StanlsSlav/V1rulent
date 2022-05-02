d705e76db3b73af8f61828978a6d20d4da155718
Stan Sebastian <stanadrian717@gmail.com>
2022-05-02 11:54:45 +0200

    Add xml parsing by simple

--- src/controller/OptionsManager.java
+++ src/controller/OptionsManager.java
@@ -4,45 +4,40 @@ package controller;
 import model.game.Difficulty;
 import model.game.Map;
 import model.game.Player;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 /**
  * Controlador para las configuraciones del juego
  */
+@Root(name = "config")
 public class OptionsManager {
     private static OptionsManager instance;
 
     public static OptionsManager getInstance() {
         if (instance == null) {
             instance = new OptionsManager();
+            instance.loadSettingsFromXml();
         }
 
         return instance;
     }
 
+    @Element(name = "player_name")
     public String playerName;
+
+    @Element(name = "difficulty")
     public Difficulty difficulty;
-    public int epidemicsThreshold = 8;
+
+    @Element(name = "epidemics_threshold")
+    public int epidemicsThreshold;
+
     public int virusesThreshold = Map.getInstance().cities.size() * 3;
 
     public SpinnerModel epidemicsLimits = new SpinnerNumberModel(1, 1, 15, 1);
@@ -58,85 +53,38 @@ public class OptionsManager {
             createDefaultXmlSettings();
         }
 
-        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder;
-        Document dom;
+        Serializer serializer = new Persister();
+        OptionsManager dummy;
 
         try {
-            docBuilder = docBuilderFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
+            dummy = serializer.read(OptionsManager.class, xmlFile);
+        } catch (Exception e) {
             e.printStackTrace();
             return;
         }
 
-        try {
-            dom = docBuilder.parse(xmlFile);
-        } catch (SAXException | IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        Element doc = dom.getDocumentElement();
-
-        Player.getInstance().setName(getTextValue(doc, "player_name"));
-        setDifficulty(Difficulty.valueOf(getTextValue(doc, "difficulty")));
-        epidemicsThreshold = Integer.parseInt(getTextValue(doc, "epidemics_threshold"));
+        Player.getInstance().setName(dummy.playerName);
+        setDifficulty(dummy.difficulty);
+        epidemicsThreshold = dummy.epidemicsThreshold;
     }
 
     private void createDefaultXmlSettings() {
         File xmlFile = new File("src/assets/settings.xml");
 
-        try {
-            xmlFile.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder;
-        Document doc;
-        Element element;
-
-        try {
-            docBuilder = docBuilderFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
+        if (xmlFile.exists() && !xmlFile.delete()) {
             return;
         }
 
-        doc = docBuilder.newDocument();
-        Element root = doc.createElement("config");
+        Serializer serializer = new Persister();
+        OptionsManager defaultOptions = new OptionsManager();
 
-        element = doc.createElement("player_name");
-        element.appendChild(doc.createTextNode(Player.getInstance().name));
-        root.appendChild(element);
-
-        element = doc.createElement("difficulty");
-        element.appendChild(doc.createTextNode(Difficulty.Easy.name()));
-        root.appendChild(element);
-
-        element = doc.createElement("epidemics_threshold");
-        element.appendChild(doc.createTextNode("8"));
-        root.appendChild(element);
-
-        doc.appendChild(root);
-
-        Transformer tr = null;
-        try {
-            tr = TransformerFactory.newInstance().newTransformer();
-        } catch (TransformerConfigurationException e) {
-            e.printStackTrace();
-        }
-
-        assert tr != null;
-        tr.setOutputProperty(OutputKeys.INDENT, "yes");
-        tr.setOutputProperty(OutputKeys.METHOD, "xml");
-        tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        tr.setOutputProperty("{http://xml.Apache.org/xslt}indent-amount", "4");
+        defaultOptions.playerName = "Unknown";
+        defaultOptions.difficulty = Difficulty.Easy;
+        defaultOptions.epidemicsThreshold = 8;
 
         try {
-            tr.transform(new DOMSource(doc), new StreamResult(new FileOutputStream(xmlFile)));
-        } catch (TransformerException | FileNotFoundException e) {
+            serializer.write(defaultOptions, xmlFile);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -144,70 +92,15 @@ public class OptionsManager {
     public void saveSettings() {
         File xmlFile = new File("src/assets/settings.xml");
 
-        try {
-            xmlFile.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder;
-        Document doc;
-        Element element;
-
-        try {
-            docBuilder = docBuilderFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
+        if (xmlFile.exists() && !xmlFile.delete()) {
             return;
         }
 
-        doc = docBuilder.newDocument();
-        Element root = doc.createElement("config");
-
-        element = doc.createElement("player_name");
-        element.appendChild(doc.createTextNode(this.playerName));
-        root.appendChild(element);
-
-        element = doc.createElement("difficulty");
-        element.appendChild(doc.createTextNode(Difficulty.Easy.name()));
-        root.appendChild(element);
-
-        element = doc.createElement("epidemics_threshold");
-        element.appendChild(doc.createTextNode(String.valueOf(epidemicsThreshold)));
-        root.appendChild(element);
-
-        doc.appendChild(root);
-
-        Transformer tr = null;
+        Serializer serializer = new Persister();
         try {
-            tr = TransformerFactory.newInstance().newTransformer();
-        } catch (TransformerConfigurationException e) {
+            serializer.write(getInstance(), xmlFile);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-        assert tr != null;
-        tr.setOutputProperty(OutputKeys.INDENT, "yes");
-        tr.setOutputProperty(OutputKeys.METHOD, "xml");
-        tr.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-        tr.setOutputProperty("{http://xml.Apache.org/xslt}indent-amount", "4");
-
-        try {
-            // FIXME: Null and produces empty xml
-            tr.transform(new DOMSource(doc), new StreamResult(new FileOutputStream(xmlFile)));
-        } catch (TransformerException | FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String getTextValue(Element doc, String tag) {
-        String value = "";
-        NodeList nl = doc.getElementsByTagName(tag);
-
-        if (nl.getLength() > 0 && nl.item(0).hasChildNodes()) {
-            value = nl.item(0).getFirstChild().getNodeValue();
-        }
-
-        return value;
     }
 }
