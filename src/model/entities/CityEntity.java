74501308c23668edf06c75276648fd06a15b1943
Stan Sebastian <stanadrian717@gmail.com>
2022-05-07 18:26:44 +0200

    Add SQL entities

---
+++ src/model/entities/CityEntity.java
@@ -0,0 +1,37 @@
+package model.entities;
+
+
+import model.game.City;
+
+import java.sql.SQLData;
+import java.sql.SQLException;
+import java.sql.SQLInput;
+import java.sql.SQLOutput;
+
+public class CityEntity implements SQLData {
+    public String name;
+    public int viruses;
+    public String typeName;
+
+    @Override
+    public String getSQLTypeName() {
+        return typeName;
+    }
+
+    @Override
+    public void readSQL(SQLInput stream, String typeName) throws SQLException {
+        this.typeName = typeName;
+        name = stream.readString();
+        viruses = stream.readInt();
+    }
+
+    @Override
+    public void writeSQL(SQLOutput stream) throws SQLException {
+        stream.writeString(name);
+        stream.writeInt(viruses);
+    }
+
+    public City toCity() {
+        return new City();
+    }
+}
---
+++ src/model/entities/PlayerEntity.java
@@ -0,0 +1,31 @@
+package model.entities;
+
+
+import java.sql.SQLData;
+import java.sql.SQLException;
+import java.sql.SQLInput;
+import java.sql.SQLOutput;
+
+public class PlayerEntity implements SQLData {
+    public String name;
+    public int actionsLeft;
+    public String typeName;
+
+    @Override
+    public String getSQLTypeName() {
+        return typeName;
+    }
+
+    @Override
+    public void readSQL(SQLInput stream, String typeName) throws SQLException {
+        this.typeName = typeName;
+        name = stream.readString();
+        actionsLeft = stream.readInt();
+    }
+
+    @Override
+    public void writeSQL(SQLOutput stream) throws SQLException {
+        stream.writeString(name);
+        stream.writeInt(actionsLeft);
+    }
+}
