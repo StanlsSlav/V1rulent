ba1e652276470cafa5c4d532cc554af021357366
Stan Sebastian <stanadrian717@gmail.com>
2022-05-13 23:01:51 +0200

    Add documentation for HexColour.java

--- src/model/base/HexColour.java
+++ src/model/base/HexColour.java
@@ -1,6 +1,9 @@
 package model.base;
 
 
+/**
+ * Represents the available colours to use within the program, in hex format
+ */
 public class HexColour {
     public static final String yellow = "#FFC90E";
     public static final String red = "#BA0003";
