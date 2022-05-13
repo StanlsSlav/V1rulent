53b44339087eff9458093e9b7054fdcad3cce44d
Sebastian Stan <stanadrian717@gmail.com>
2022-05-13 16:29:11 +0200

    Add documentation for Panel.java

--- src/model/base/Panel.java
+++ src/model/base/Panel.java
@@ -5,6 +5,9 @@ import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Image;
 
+/**
+ * Enhanced {@link JPanel} to support background images
+ */
 public class Panel extends JPanel {
     public Panel(String backgroundImageName) {
         setBackgroundImage(getToolkit().getImage("src/assets/img/" + backgroundImageName + ".png"));
